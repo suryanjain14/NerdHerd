bc5b03ff4ed1b50de6fd487c8cf7700946399ddd
Anmol Pal <anmolpal1999@gmail.com>
2019-09-04 01:51:58 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -181,7 +181,7 @@ $(window).on('load', function () {
 
     // 1. Render Map
     var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 15,
+        zoom: 13,
         center: myLatlng
     });
 
